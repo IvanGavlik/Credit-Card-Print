36149100dfa32e8e8a8fc5f94133bcd0def79e78
ivan <ivangavlik963@gmail.com>
2023-06-14 23:24:59 +0200

    init

---
+++ src/main/java/hr/rba/creditcardissuing/CreditCardIssuingApplication.java
@@ -0,0 +1,13 @@
+package hr.rba.creditcardissuing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardIssuingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditCardIssuingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hr/rba/creditcardissuing/CreditCardIssuingApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.rba.creditcardissuing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardIssuingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
